efc8e85ee1c3ec1cc02bf7c2ae11c558770ac141
smukx18 <120155672+smukx18@users.noreply.github.com>
2023-10-11 18:48:55 +0530

    Update windows-10-Activator.ps1

--- windows-10-Activator.ps1
+++ windows-10-Activator.ps1
@@ -53,7 +53,139 @@ cd /d "%~dp0"
 cls
 call :Header "WINDOWS 10 DIGITAL LICENSE [Windows 10 %osedition% %vera%]"
 echo:
-
+if [%osedition%] == [Cloud] (
+	set "edition=Cloud"
+	set "key=V3WVW-N2PV2-CGWC3-34QGF-VMJ2C"
+	set "sku=178"
+	set "editionId=X21-32983"
+	goto :parseAndPatch
+)
+if [%osedition%] == [CloudN] (
+	set "edition=CloudN"
+	set "key=NH9J3-68WK7-6FB93-4K3DF-DJ4F6"
+	set "sku=179"
+	set "editionId=X21-32987"
+	goto :parseAndPatch
+)
+if [%osedition%] == [Core] (
+	set "edition=Core"
+	set "key=YTMG3-N6DKC-DKB77-7M9GH-8HVX7"
+	set "sku=101"
+	set "editionId=X19-98868"
+	goto :parseAndPatch
+)
+if [%osedition%] == [CoreCountrySpecific] (
+	set "edition=CoreCountrySpecific"
+	set "key=N2434-X9D7W-8PF6X-8DV9T-8TYMD"
+	set "sku=99"
+	set "editionId=X19-99652"
+	goto :parseAndPatch
+)
+if [%osedition%] == [CoreN] (
+	set "edition=CoreN"
+	set "key=4CPRK-NM3K3-X6XXQ-RXX86-WXCHW"
+	set "sku=98"
+	set "editionId=X19-98877"
+	goto :parseAndPatch
+)
+if [%osedition%] == [CoreSingleLanguage] (
+	set "edition=CoreSingleLanguage"
+	set "key=BT79Q-G7N6G-PGBYW-4YWX6-6F4BT"
+	set "sku=100"
+	set "editionId=X19-99661"
+	goto :parseAndPatch
+)
+if [%osedition%] == [Education] (
+	set "edition=Education"
+	set "key=YNMGQ-8RYV3-4PGQ3-C8XTP-7CFBY"
+	set "sku=121"
+	set "editionId=X19-98886"
+	goto :parseAndPatch
+)
+if [%osedition%] == [EducationN] (
+	set "edition=EducationN"
+	set "key=84NGF-MHBT6-FXBX8-QWJK7-DRR8H"
+	set "sku=122"
+	set "editionId=X19-98892"
+	goto :parseAndPatch
+)
+if [%osedition%] == [Enterprise] (
+	set "edition=Enterprise"
+	set "key=XGVPP-NMH47-7TTHJ-W3FW7-8HV2C"
+	set "sku=4"
+	set "editionId=X19-99683"
+	goto :parseAndPatch
+)
+if [%osedition%] == [EnterpriseN] (
+	set "edition=EnterpriseN"
+  set "key=WGGHN-J84D6-QYCPR-T7PJ7-X766F"
+	set "sku=27"
+	set "editionId=X19-98746"
+	goto :parseAndPatch
+)
+if [%osedition%] == [EnterpriseS] (
+	set "edition=EnterpriseS"
+	set "key=NK96Y-D9CD8-W44CQ-R8YTK-DYJWX"
+	set "sku=125"
+	set "editionId=X21-05035"
+	goto :parseAndPatch
+)
+if [%osedition%] == [EnterpriseSN] (
+	set "edition=EnterpriseSN"
+	set "key=RW7WN-FMT44-KRGBK-G44WK-QV7YK"
+	set "sku=126"
+	set "editionId=X21-04921"
+	goto :parseAndPatch
+)
+if [%osedition%] == [Professional] (
+	set "edition=Professional"
+	set "key=VK7JG-NPHTM-C97JM-9MPGT-3V66T"
+	set "sku=48"
+	set "editionId=X19-98841"
+	goto :parseAndPatch
+)
+if [%osedition%] == [ProfessionalEducation] (
+	set "edition=ProfessionalEducation"
+	set "key=8PTT6-RNW4C-6V7J2-C2D3X-MHBPB"
+	set "sku=164"
+	set "editionId=X21-04955"
+	goto :parseAndPatch
+)
+if [%osedition%] == [ProfessionalEducationN] (
+	set "edition=ProfessionalEducationN"
+	set "key=GJTYN-HDMQY-FRR76-HVGC7-QPF8P"
+	set "sku=165"
+	set "editionId=X21-04956"
+	goto :parseAndPatch
+)
+if [%osedition%] == [ProfessionalN] (
+	set "edition=ProfessionalN"
+	set "key=2B87N-8KFHP-DKV6R-Y2C8J-PKCKT"
+	set "sku=49"
+	set "editionId=X19-98859"
+	goto :parseAndPatch
+)
+if [%osedition%] == [ProfessionalWorkstation] (
+	set "edition=ProfessionalWorkstation"
+	set "key=DXG7C-N36C4-C4HTG-X4T3X-2YV77"
+	set "sku=161"
+	set "editionId=X21-43626"
+	goto :parseAndPatch
+)
+if [%osedition%] == [ProfessionalWorkstationN] (
+	set "edition=ProfessionalWorkstationN"
+	set "key=WYPNQ-8C467-V2W6J-TX4WX-WT2RQ"
+	set "sku=162"
+	set "editionId=X21-43644"
+	goto :parseAndPatch
+)
+if [%osedition%] == [ServerRdsh] (
+	set "edition=ServerRdsh"
+	set "key=NJCF7-PW8QT-3324D-688JX-2YV66"
+	set "sku=175"
+	set "editionId=X21-41295"
+	goto :parseAndPatch
+)
 ::===============================================================================================================
 :parseAndPatch
 cls
